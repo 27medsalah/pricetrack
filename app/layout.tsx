3bb50a3a906380fe333feed65c8c4c11a2e7dbef
27medsalah <aellgarhyy@gmail.com>
2024-08-03 15:36:12 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
